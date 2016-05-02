8816b367298b3d00a6019a5a6e8ffa298baeebb5
thulioph <thulioph@gmail.com>
2016-05-02 01:45:48 -0300

    Redireciona o usuário após validar a denúncia.

--- app/scripts/controllers/validar.js
+++ app/scripts/controllers/validar.js
@@ -14,11 +14,13 @@ angular.module('angryTaxiApp')
 
     if (ls === null) {
       Notification.show('Atenção!', 'Ocorreu um erro, tente novamente em alguns instantes.');
+      _redirectUser();
     } else {
       if ($routeParams.id === ls._id) {
         _checkComplaint(ls._id);
       } else {
         Notification.show('Atenção!', 'A url parece não ser válida. Por favor, tente novamente.');
+        _redirectUser();
       }
     }
 
@@ -29,13 +31,17 @@ angular.module('angryTaxiApp')
         if (data.status === 201) {
           LocalStorage.saveComplaint(data.data);
           Notification.show('Denúncia confirmada com sucesso!', 'Obrigado :)');
-
-          $timeout(function() {
-            $location.path('/');
-          }, 3000);
         } else {
           Notification.show('Atenção!', 'Tivemos um problema, tente novamente em alguns instantes.');
         }
+
+        _redirectUser();
       })
+    }
+
+    function _redirectUser() {
+      $timeout(function() {
+        $location.path('/');
+      }, 3000);
     }
   });
